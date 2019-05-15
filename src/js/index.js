428e610f29c243f28fff25fecc7624dfb1516276
pelevin-d-y <natpuot1992@gmail.com>
2019-05-16 02:03:11 +0300

    first commit

---
+++ src/js/index.js
@@ -0,0 +1,45 @@
+// 'use strict';
+
+// var toggle = document.querySelector('.toggle');
+// var toggleCircle = document.querySelector('.toggle__circle');
+
+// toggle.addEventListener('click', function() {
+//   toggle.classList.toggle('active');
+// });
+
+
+// var switcherRightArrow = document.querySelector('.switcher__right');
+// var switcherLeftArrow = document.querySelector('.switcher__left');
+// var switcherType = document.querySelector('.switcher__type');
+// var switcherTypes = ['Yesterday', 'Today', 'Tomorrow'];
+
+// var switchDay = function(dir) {
+//   switch (true) {
+//     case switcherType.textContent === 'Today':
+//       switcherType.textContent = dir === 'next' ? 'Tomorrow' : 'Yesterday'
+//       break;
+//     case switcherType.textContent === 'Tomorrow':
+//       switcherType.textContent = dir === 'next' ? 'Yesterday' : 'Today'
+//       break;
+//     case switcherType.textContent === 'Yesterday':
+//       switcherType.textContent = dir === 'next' ? 'Today' : 'Tomorrow'
+//       break;
+//     default:
+//       break;
+//   }
+// }
+
+// switcherRightArrow.addEventListener('click', function() {
+//   switchDay('next');
+// })
+
+// switcherLeftArrow.addEventListener('click', function() {
+//   switchDay('prev');
+// })
+
+// var burger = document.querySelector('.header__burger')
+// var header = document.querySelector('.header')
+
+// burger.addEventListener('click', function() {
+//   header.classList.toggle('menu-open')
+// })
