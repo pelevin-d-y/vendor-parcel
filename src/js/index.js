2c1e3d3f95edd12cbce4fbcf0c36b823c497884e
pelevin-d-y <natpuot1992@gmail.com>
2019-05-16 23:06:58 +0300

    added login page

--- src/js/index.js
+++ src/js/index.js
@@ -1,45 +1,64 @@
-// 'use strict';
+'use strict';
 
-// var toggle = document.querySelector('.toggle');
-// var toggleCircle = document.querySelector('.toggle__circle');
+import Choices from 'choices.js'
 
-// toggle.addEventListener('click', function() {
-//   toggle.classList.toggle('active');
-// });
+var toggle = document.querySelector('.toggle');
+if (toggle) {
+  toggle.addEventListener('click', function() {
+    toggle.classList.toggle('active');
+  });
+}
 
+var switcherRightArrow = document.querySelector('.switcher__right');
+var switcherLeftArrow = document.querySelector('.switcher__left');
+var switcherType = document.querySelector('.switcher__type');
 
-// var switcherRightArrow = document.querySelector('.switcher__right');
-// var switcherLeftArrow = document.querySelector('.switcher__left');
-// var switcherType = document.querySelector('.switcher__type');
-// var switcherTypes = ['Yesterday', 'Today', 'Tomorrow'];
+var switchDay = function(dir) {
+  switch (true) {
+    case switcherType.textContent === 'Today':
+      switcherType.textContent = dir === 'next' ? 'Tomorrow' : 'Yesterday'
+      break;
+    case switcherType.textContent === 'Tomorrow':
+      switcherType.textContent = dir === 'next' ? 'Yesterday' : 'Today'
+      break;
+    case switcherType.textContent === 'Yesterday':
+      switcherType.textContent = dir === 'next' ? 'Today' : 'Tomorrow'
+      break;
+    default:
+      break;
+  }
+}
+if (switcherRightArrow || switcherLeftArrow) {
+  switcherRightArrow.addEventListener('click', function() {
+    switchDay('next');
+  })
 
-// var switchDay = function(dir) {
-//   switch (true) {
-//     case switcherType.textContent === 'Today':
-//       switcherType.textContent = dir === 'next' ? 'Tomorrow' : 'Yesterday'
-//       break;
-//     case switcherType.textContent === 'Tomorrow':
-//       switcherType.textContent = dir === 'next' ? 'Yesterday' : 'Today'
-//       break;
-//     case switcherType.textContent === 'Yesterday':
-//       switcherType.textContent = dir === 'next' ? 'Today' : 'Tomorrow'
-//       break;
-//     default:
-//       break;
-//   }
-// }
+  switcherLeftArrow.addEventListener('click', function() {
+    switchDay('prev');
+  })
+}
 
-// switcherRightArrow.addEventListener('click', function() {
-//   switchDay('next');
-// })
+var burger = document.querySelector('.header__burger')
+var header = document.querySelector('.header')
 
-// switcherLeftArrow.addEventListener('click', function() {
-//   switchDay('prev');
-// })
+if (burger) {
+  burger.addEventListener('click', function() {
+    header.classList.toggle('menu-open')
+  })
+}
 
-// var burger = document.querySelector('.header__burger')
-// var header = document.querySelector('.header')
+const select = document.querySelector('.login-select')
 
-// burger.addEventListener('click', function() {
-//   header.classList.toggle('menu-open')
-// })
+if (select) {
+  const example = new Choices(select, {
+      searchEnabled: false,
+      placeholderValue: 'Location',
+    });
+  
+  // example.setChoices([
+  //   {value: 'location1', label: 'location1'},
+  //   {value: 'location2', label: 'location2', selected: true},
+  //   {value: 'location3', label: 'location3'},
+  // ]);
+
+}
